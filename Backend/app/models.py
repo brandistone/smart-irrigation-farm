de3c7c7b56ba91ee15080a4719662c0cc419e6a0
Brandistone Mabeya <165170666+brandistone@users.noreply.github.com>
2024-12-17 16:50:45 +0300

    Update models.py

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     username = models.CharField(max_length=30)
-    Phone = models.CharField(max_length=10)
+    fullname = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    phonenumber = models.CharField(max_length=10)
+    country = models.CharField(max_length=10)
     email = models.EmailField(max_length=50)
     
+    # fields = ['id', 'username', 'password', 'fullname', 'email', 'phonenumber','country' ]
+    
     def __str__(self):
-        return self.Username
+        return self.username
